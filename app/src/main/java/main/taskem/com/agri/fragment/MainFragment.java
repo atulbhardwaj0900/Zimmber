90b8fed72ea6e2f514d6bb582fd1feca3be37c3a
Atul Bhardwaj <atul.bhardwaj@snapdeal.com>
2016-06-03 21:22:00 +0530

    Removed code from Mainfrgament

--- app/src/main/java/main/taskem/com/agri/fragment/MainFragment.java
+++ app/src/main/java/main/taskem/com/agri/fragment/MainFragment.java
@@ -2,7 +2,6 @@ package main.taskem.com.agri.fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,11 +12,6 @@ import org.json.JSONObject;
 import main.taskem.com.agri.R;
 import main.taskem.com.agri.adapter.JsonArrayAdapter.OnRecyclerItemClick;
 import main.taskem.com.agri.controller.Controller;
-import main.taskem.com.agri.models.BodyContent;
-import main.taskem.com.agri.models.HeadContent;
-import main.taskem.com.agri.view.EventDetailsView;
-import main.taskem.com.agri.view.EventHeaderView;
-import main.taskem.com.agri.view.EventImageView;
 
 /**
  * Created by atul.bhardwaj on 30/05/16.
@@ -42,43 +36,8 @@ public class MainFragment extends BaseFragment implements OnRecyclerItemClick<JS
 		super.onActivityCreated(savedInstanceState);
 		Controller controller = new Controller(getContext());
 		controller.loadContent();
-		setBackgroundImg(controller.getImage());
-		addHeadView(controller.getHeadContent());
-		addEventDetailsView(controller.getBodyContent());
 	}
 
-
-	private void addEventDetailsView(BodyContent bodyContent) {
-		if (bodyContent != null) {
-			EventDetailsView eventDetailsView = new EventDetailsView(mBaseActivity);
-			eventDetailsView.setData(bodyContent);
-			mFragmentView.addView(eventDetailsView);
-		}
-	}
-
-
-	private void setBackgroundImg(String url) {
-		if (!TextUtils.isEmpty(url)) {
-			EventImageView eventImageView =
-					(EventImageView) mBaseActivity.findViewById(R.id.backgroundImageView);
-			assert eventImageView != null;
-			eventImageView.setImage(url);
-		}
-	}
-
-	private void addHeadView(HeadContent headContent) {
-
-		EventHeaderView eventHeaderView =
-				(EventHeaderView) mFragmentView.findViewById(R.id.headerView);
-		if (headContent == null) {
-			eventHeaderView.setVisibility(View.GONE);
-		} else {
-			eventHeaderView.setHeading(headContent.getHeading());
-			eventHeaderView.setStartDate(headContent.getStartDate());
-			eventHeaderView.setEndDate(headContent.getEndDate());
-			eventHeaderView.setAttendingStatus(headContent.getAttendingStatus());
-		}
-	}
 
 	@Override
 	public void onClick(View v) {
